955549ae384d614df7043c0480006f68bed826c7
Melquíades Mário <melquiadesm@gmail.com>
2025-10-20 23:35:54 -0300

    feat: learning componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+interface ICardprops{
+  title: string
+  children: React.ReactNode
+}
+
+const Card = (props: ICardprops) => {
+  return(
+    <div style={{ border: '1px solid black'}}>
+      <span>Title: { props.title }</span>
+      <div>
+        { props.children }
+      </div>
+      <div>
+        Footer
+      </div>
+    </div>
+  )
+}
+
 
 export function App() {
 
   return (
-    <>
+    <div>
       Ola
-    </>
+
+      <p>Card:</p>
+      <Card title='teste'>
+        Teste
+      </Card>
+    </div>
   )
 }
